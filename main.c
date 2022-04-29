f74610ab45cc3b9d35b154d87802d8a61304ba59
Capta1n-n9m0 <alievabbas1@gmail.com>
2022-04-30 01:45:08 +0400

    Started Smith-Waterman algorithm

--- main.c
+++ main.c
@@ -1,6 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "blast-core.h"
+#include <limits.h>
+
+int max(const int arr[], int n){
+	int res = 0;
+	for (int i = 0; i < n; ++i)
+		if(arr[i] > res) res = arr[i];
+	return res;
+}
+
+typedef struct node_{
+	int col, row, val;
+}node;
+
+typedef struct matrix_{
+	int width, height;
+	node maximum;
+	int **data;
+} matrix;
+
+matrix SW_search(const char *data, const char *target){
+	matrix res;
+	int data_length = strlen(data), target_length = strlen(target);
+	res.width = data_length;
+	res.height = target_length;
+	int **pmatrix = malloc(target_length * sizeof (int *));
+	for(int row = 0; row < target_length+1; row++)
+		pmatrix[row] = calloc(data_length + 1, sizeof(int));
+	int maximum = 0;
+	for(int i = 0; i < data_length; i++) pmatrix[0][i] = 0;
+	for(int i = 0; i < target_length; i++) pmatrix[i][0] = 0;
+	for(int row = 1; row < target_length; row++){
+		for(int col = 1; col < data_length; col++){
+			int diag = pmatrix[row - 1][col - 1];
+			if(data[col] == target[row]) diag += 3; else diag -=3;
+			int left = pmatrix[row][col - 1] - 2;
+			int up = pmatrix[row - 1][col] - 2;
+			pmatrix[row][col] = max((int[]){diag, left, up}, 3);
+			if(pmatrix[row][col] > maximum) {
+				res.maximum.col = col;
+				res.maximum.row = row;
+				maximum = pmatrix[row][col];
+			}
+		}
+	}
+	res.data = pmatrix;
+	res.maximum.val = maximum;
+	return res;
+}
+
+typedef struct sw_result_{
+	char *v1, *v2;
+	int score;
+}sw_result;
+
+typedef struct linked_node_{
+	char value;
+	struct linked_node_ *next;
+}linked_node;
+
+linked_node *add_to_beginning_of_linked_list(linked_node *ll, char c){
+	linked_node *res = calloc(1, sizeof(linked_node));
+	res->next = ll;
+	res->value = c;
+	return res;
+}
+
+sw_result sw_backtrack(matrix m, char *data, char *target){
+	sw_result res;
+	int length_target = strlen(target);
+	res.v1 = calloc(length_target*2+1, sizeof(char ));
+	res.v2 = calloc(length_target*2+1, sizeof(char ));
+	for(int row = m.maximum.row, col = m.maximum.col; m.data[row][col];){
+	
+	}
+	res.score = m.maximum.val;
+	return res;
+}
+
+void test_sw(){
+	const char *dna = "TGTTACGG";
+	const char *searchedPart = "GGTTGACTA";
+	matrix res = SW_search(dna, searchedPart);
+	for(int row = 0; row < res.height; row++){
+		for (int col = 0; col < res.width; ++col) {
+			printf("%3d ", res.data[row][col]);
+		}
+		puts("");
+	}
+	printf("(col=%d, row=%d) = %d", res.maximum.col, res.maximum.row, res.maximum.val);
+}
 
 void test_file() {
 	FILE *pFasta = fopen("DNACervisiaeSaccharomyces.txt.oneline", "r");
@@ -33,7 +124,8 @@ void test_matches() {
 }
 
 int main() {
-	test_matches();
-	test_file();
+//	test_matches();
+//	test_file();
+	test_sw();
 }
 
